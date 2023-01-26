e644c770ba019a6db42ceecc19bbb63b5ecd385d
root <root@deneb01.codefactory.levigo.de>
2023-01-26 23:50:22 +0100

    grafana fluent dashboard added + fluent config adjustmant

--- loki-fluent-example/clear.sh
+++ loki-fluent-example/clear.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR_REL=$(dirname ${BASH_SOURCE[0]})
 
-docker-compose -f $SCRIPT_DIR_REL/app/nginx-example.yaml down |
+docker-compose -f $SCRIPT_DIR_REL/app/nginx-example.yaml down &&
   docker-compose -f $SCRIPT_DIR_REL/docker-compose.yml down

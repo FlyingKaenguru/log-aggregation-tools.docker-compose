b987361f19122b08368b6eb352b1728e6bbd59a9
root <root@deneb01.codefactory.levigo.de>
2023-01-26 23:55:45 +0100

    minimal dashboard changes - Promtail

--- loki-promtail-example/run.sh
+++ loki-promtail-example/run.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR_REL=$(dirname ${BASH_SOURCE[0]})
 
-docker-compose -f $SCRIPT_DIR_REL/docker-compose.yml up -d |
+docker-compose -f $SCRIPT_DIR_REL/docker-compose.yml up -d &&
   docker-compose -f $SCRIPT_DIR_REL/app/nginx-example.yaml up -d
